dc02a885be9a3383e0d8186176990796fb2bff00
Nathan Anderson <nathand.anderson@outlook.com>
2022-02-14 19:33:19 -0700

    Add project files.

---
+++ Models/Projects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WaterProject.Models
+{
+    public partial class Projects
+    {
+        public long ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectType { get; set; }
+        public long? ProjectRegionalProgram { get; set; }
+        public long? ProjectImpact { get; set; }
+        public string ProjectPhase { get; set; }
+        public string ProjectFunctionalityStatus { get; set; }
+    }
+}
