b2e8cec60d4de5ae87b84327f55cc32510a044f1
Nathan Anderson <andernat@byu.edu>
2022-03-09 18:53:21 -0700

    Videos 1-4

--- Models/EFWaterProjectRepository.cs
+++ Models/EFWaterProjectRepository.cs
@@ -14,5 +14,22 @@ namespace WaterProject.Models
             context = temp;
         }
         public IQueryable<Project> Projects => context.Projects;
+
+        public void SaveProject(Project p)
+        {
+            context.SaveChanges();
+        }
+
+        public void CreateProject(Project p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+        }
+
+        public void DeleteProject(Project p)
+        {
+            context.Remove(p);
+            context.SaveChanges();
+        }
     }
 }
